adc8890db567a077d424a3d275e69aad364b6828
NISHIMURA-MieMatsusaka-Kazunari <kaz_nisi@mac.com>
2025-06-08 10:09:13 +0900

    Fixed BskyChat.pm
    Fixed BskyChat.pm 20250608 10:10

--- AtProtocol/BskyChat.pm
+++ AtProtocol/BskyChat.pm
@@ -781,8 +781,8 @@ sub sendMessage {
 	my $ret					= undef;
 	eval{
 		$atProto->getAccessToken()										or die("Err getAccessToken: $atProto->{err}");#start exclusive control
-		if($handle =~ /^\@(.+)$/){
-			my $did = $atProto->resolveHandle($1)						or die("Cannot resolveHandle: $handle");
+		if($handleOrConvoId =~ /^\@(.+)$/){
+			my $did = $atProto->resolveHandle($1)						or die("Cannot resolveHandle: $handleOrConvoId");
 			my $convo = $atProto->convo_getConvoAvailability($did) 		or die("Cannot get convo0: $did");
 			if(defined($convo->{convo}{id})){
 				$convoId = $convo->{convo}{id};
